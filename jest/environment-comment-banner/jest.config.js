09fb270a810f8c67874f31eed011966edc6227e5
colder <cncolder@gmail.com>
2020-11-30 01:00:35 +0800

    fix: output jest html report to dist

--- jest/environment-comment-banner/jest.config.js
+++ jest/environment-comment-banner/jest.config.js
@@ -1,22 +1,6 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
-    reporters: [
-        'default',
-        ['jest-html-reporter', { outputPath: './public/index.html' }],
-        // ['jest-html-reporters', { expand: true, filename: 'index.html', hideIcon: true, publicPath: 'public' }],
-        // [
-        //     'jest-stare',
-        //     {
-        //         disableCharts: true,
-        //         hidePassing: true,
-        //         log: 'false',
-        //         resultDir: 'public',
-        //         reportHeadline: 'Jest',
-        //         reportSummary: true,
-        //         reportTitle: 'Jest',
-        //     },
-        // ],
-    ],
+    reporters: ['default', ['jest-html-reporter', { outputPath: './dist/index.html' }]],
     /** @default 'jsdom' */
     testEnvironment: 'jsdom',
     verbose: true,
