6cb5971ece4020928abe259182bc6168ecd83e49
colder <cncolder@gmail.com>
2020-11-30 10:17:02 +0800

    feat: add more detail for react17 jsx factories

--- typescript/version-4-1-react-17-jsx-factories/src/App.tsx
+++ typescript/version-4-1-react-17-jsx-factories/src/App.tsx
@@ -1,1 +1,14 @@
-export default () => <div>"jsx": "react-jsx"</div>;
+export default () => (
+    <div>
+        <h1>React 17 JSX Factories</h1>
+        <p>
+            TypeScript 4.1 supports React 17’s upcoming jsx and jsxs factory functions through two new options for the
+            jsx compiler option:
+            <ul>
+                <li>react-jsx</li>
+                <li>react-jsxdev</li>
+            </ul>
+        </p>
+        <code>{`{ "compilerOptions": { "jsx": "react-jsx" } }`}</code>
+    </div>
+);
