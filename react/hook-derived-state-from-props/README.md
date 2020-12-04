1e3cb96cd292825e6bb76c1f27ae6acd72c4a8a2
colder <cncolder@gmail.com>
2020-12-04 20:42:58 +0800

    feat: add react/hook-derived-state-from-props

---
+++ react/hook-derived-state-from-props/README.md
@@ -0,0 +1,7 @@
+# React hook derived state from props
+
+[![](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/cncolder/demo/master/shields/codesandbox.json)](https://githubbox.com/cncolder/demo/tree/master/react/hook-derived-state-from-props)
+
+## Links
+
+https://reactjs.org/docs/hooks-faq.html#how-do-i-implement-getderivedstatefromprops
---
+++ react/hook-derived-state-from-props/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useCallback, useState } from 'react';
+import { useDerivedState } from './useDerivedState';
+
+interface PrimarySchoolNumberInputProps {
+    value: number;
+    onChange(value: number): void;
+}
+
+const PrimarySchoolNumberInput: FC<PrimarySchoolNumberInputProps> = (props) => {
+    const [value, setValue] = useDerivedState(props.value);
+
+    const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+        (e) => {
+            const val = e.target.value;
+
+            setValue(val as any);
+
+            const num = Number.parseInt(val);
+
+            if (val === num.toString() && num >= 0 && num <= 10) {
+                props.onChange(num);
+            }
+        },
+        [props.onChange]
+    );
+
+    return <input type="text" value={value} onChange={onChange} />;
+};
+
+export default () => {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <h1>How do I implement getDerivedStateFromProps?</h1>
+
+            <h3>Current value: {value}</h3>
+
+            <p>
+                <label>
+                    Controlled Range Input (0-10):
+                    <input
+                        type="range"
+                        min="0"
+                        max="10"
+                        value={value}
+                        onChange={(e) => setValue(parseInt(e.target.value, 10))}
+                    />
+                </label>
+            </p>
+
+            <p>
+                <label>
+                    Derived Number Input (0-10): <PrimarySchoolNumberInput value={value} onChange={setValue} />
+                </label>
+            </p>
+        </div>
+    );
+};
